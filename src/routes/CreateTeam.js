7e5bd3b4ec2a869a5230f4ecfce6238d5ff9183e
businessOS <businessOS@iCloud.com>
2018-02-24 12:11:38 -0400

    Interaccion Team (Seleccion) y channels (Creacion)

--- src/routes/CreateTeam.js
+++ src/routes/CreateTeam.js
@@ -28,10 +28,10 @@ class CreateTeam extends React.Component {
       return;
     }
 
-    const { ok, errors } = response.data.createTeam;
+    const { ok, errors, team } = response.data.createTeam;
 
     if (ok) {
-      this.props.history.push('/');
+      this.props.history.push(`/view-team/${team.id}`);
     } else {
       const err = {};
       errors.forEach(({ path, message }) => {
@@ -76,7 +76,11 @@ class CreateTeam extends React.Component {
 const createTeamMutation = gql`
     mutation($name: String!) {
       createTeam(name: $name) {
-        ok
+        ok,
+        team {
+          id,
+          name
+        },
         errors {
           path
           message
--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -1,28 +1,23 @@
 import React from 'react';
 
-import Channels from '../components/Channels';
-import Teams from '../components/Teams';
 import Header from '../components/Header';
 import Messages from '../components/Messages';
 import SendMessage from '../components/SendMessage';
 import AppLayout from '../components/AppLayout';
+import Sidebar from '../containers/Sidebar';
 
-export default () => (
+const ViewTeam = ({ match: { params } }) => (
   <AppLayout>
-    <Teams teams={[{ id: 1, letter: 'B' }, { id: 2, letter: 'Q' }]} />
-    <Channels
-      teamName="Team Name"
-      username="UserName"
-      channels={[{ id: 1, name: 'General' }, { id: 2, name: 'Random' }]}
-      users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
-    />
-    <Header channelName="General" />
+    <Sidebar currentTeamId={params.teamId} />
+    <Header channelName="general" />
     <Messages>
       <ul className="message-list">
         <li />
         <li />
       </ul>
     </Messages>
-    <SendMessage channelName="General" />
+    <SendMessage channelName="general" />
   </AppLayout>
 );
+
+export default ViewTeam;
